8b708aa9fde634ec85d139bda1a56dd3833a196f
jopaul23 <jopauljoy999@gmail.com>
2021-04-08 19:33:30 +0530

    Output created

--- MaxFinder.py
+++ MaxFinder.py
@@ -12,6 +12,8 @@ def list_to_date(date_list):
 
 
 if __name__ =="__main__":
+    starting_year = 2004
+    ending_year = 2021
     filename = "./Weather.csv"
 
     df = pd.read_csv (filename)   #read the csv file (put 'r' before the path string to address any special characters in the path, such as '\'). Don't forget to put the file name at the end of the path + ".csv"
@@ -21,7 +23,7 @@ if __name__ =="__main__":
     for index,row in df.iterrows():
         date_string =row['Date'].split('/')
         date_string.remove(date_string[1])
-        if(date_string[0]!=final_list[len(final_list)-1]["Date"][0]) and int(date_string[1])>=2004:
+        if((date_string[0]!=final_list[len(final_list)-1]["Date"][0]) and int(date_string[1])>=starting_year)and  int(date_string[1])<=ending_year:
             final_list.append({"Date" :date_string,"Max Temperature":row["Max Temperature"],"Min Temperature":row["Min Temperature"],"Precipitation":row["Precipitation"],"Wind":row["Wind"],"Relative Humidity":row["Relative Humidity"],"Solar":row["Solar"]})
         else:
             if final_list[len(final_list)-1]["Max Temperature"]<row['Max Temperature']:
